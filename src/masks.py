9cb8213388dd09ea2e3f5de3b18be0f1469e52ec
Alexandr <kokarev17@gmail.com>
2025-02-16 19:45:48 +0300

    Дописан код и сделаны теcты для модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,15 @@
 def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер банковской карты"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    card_str = str(card_number)
+    if len(card_str) != 16:
+        raise ValueError
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
+print(get_mask_card_number("7000792289606361"))
 
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер банковского счета"""
+    acc_num = str(account_number)
+    if len(acc_num) != 20:
+        raise ValueError
     return f"**{account_number[-4:]}"
---
+++ tests/test_masks.py
@@ -0,0 +1,39 @@
+import pytest
+
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+@pytest.mark.parametrize(
+    "card_num, expected",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("4758264516284964", "4758 26** **** 4964"),
+        ("9285619274858732", "9285 61** **** 8732"),
+    ],
+)
+def test_get_mask_card_number(card_num, expected) -> None:
+    assert get_mask_card_number(card_num) == expected
+
+
+def test_invalid_get_mask_card_number() -> None:
+    with pytest.raises(ValueError):
+         assert get_mask_card_number("")
+         assert get_mask_card_number(125125452626136426)
+         assert get_mask_card_number("ewqg")
+
+
+@pytest.mark.parametrize(
+    "acc_num, expected",
+    [
+        ("73654108430135874305", "**4305"),
+        ("23566455472347343734", "**3734"),
+    ],
+)
+def test_get_mask_account(acc_num, expected) -> None:
+    assert get_mask_account(acc_num) == expected
+
+
+def test_invalid_get_mask_account() -> None:
+    with pytest.raises(ValueError):
+        assert get_mask_account("275275253")
+        assert get_mask_account("weguwg")
