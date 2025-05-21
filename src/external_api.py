03c6970f18ea77d7279765d206e1f8c75616561f
Alexandr <kokarev17@gmail.com>
2025-05-21 15:14:05 +0300

    Правки

--- src/external_api.py
+++ src/external_api.py
@@ -1,10 +1,8 @@
 import os
-
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 def convert_to_rub(transaction: dict) -> float:
     """Конвертирует сумму транзакции в рубли.
@@ -25,9 +23,10 @@ def convert_to_rub(transaction: dict) -> float:
     url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
     headers = {"apikey": os.getenv("API_KEY")}
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
         return float(response.json()["result"])
-    else:
-        return float(amount)
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        raise ValueError("Ошибка при получении курса валюты") from e
